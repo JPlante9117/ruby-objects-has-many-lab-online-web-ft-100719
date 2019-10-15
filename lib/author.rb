6b90e66cf887f3c1417af38698392f57b5bb66bd
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-15 14:54:02 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -4,24 +4,24 @@ class Author
   
   def initialize(name)
     @name = name        #gives the author a name
-    @posts = []         #
+    @posts = []         #creates an array of the current author's posts
   end
   
   def posts 
     Post.all.select { |post| post.author == self }  #selecting all posts by this author from the total post list
   end
   
-  def add_post(given_post)                          #
+  def add_post(given_post)    #takes a given post, associates it with the author, and adds it to the array of posts
     @posts << given_post
     given_post.author = self
   end
   
-  def add_post_by_title(title)
+  def add_post_by_title(title)  #takes a given post name, intantiates a new post with that name, and then runs the add_post method
     new_post = Post.new(title)
     add_post(new_post)
   end
   
-  def self.post_count
+  def self.post_count           #references @@all from the Post class which has ALL posts in it.
     Post.all.length
   end
   
